e3d4a688ae232ee2baa0482a601406c7fc3b6c73
Ryan <leehahnwoong@gmail.com>
2025-01-11 20:27:42 -0800

    test

--- dind/docker_dind.py
+++ dind/docker_dind.py
@@ -20,7 +20,7 @@ def run_container(num_containers, username):
                 "traefik.enable": "true",
                 "traefik.http.routers.dind.rule": f"HostRegexp(`{username}.dind.localhost:{{port:[0-9]+}}`)",
                 "traefik.http.services.dind.loadbalancer.server.scheme": "http",
-                "traefik.http.services.dind.loadbalancer.server.port": "80",
+                "traefik.http.services.dind.loadbalancer.server.port": "{port}",
                 "traefik.http.middlewares.dind-replacepathregex.replacepathregex.regex": "^/.*",
                 "traefik.http.middlewares.dind-replacepathregex.replacepathregex.replacement": "/"
             },
@@ -30,14 +30,17 @@ def run_container(num_containers, username):
     except docker.errors.APIError as e:
         print(f"An error occurred: {e}")
 
-def run_docker_compose():
+def run_docker_compose(container_name, repo_name):
     client = docker.from_env()
-    container = client.containers.list()
-    for i in container:
-        print(i.name)
+    # container = client.containers.list()
+    container = client.containers.get(container_name)
+    command = f"git clone https://github.com/your/repo.git && \ cd repo && \ docker-compose up -d"
+    exec_code, output = container.exec_run(command, detach=True)
+    print(f"Code: {exec_code}")
+    print("Output:", output.decode("utf-8"))
 
 if __name__ == "__main__":
     NUM_CONTAINERS = 1
     username = input("Enter your username: ")
-    run_container(NUM_CONTAINERS, username)
-    run_docker_compose()
+    run_container(NUM_CONTAINERS, "")
+    run_docker_compose(f"dind-{NUM_CONTAINERS}", )
